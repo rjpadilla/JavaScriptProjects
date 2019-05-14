d9860d22bf305933879f72dccb04aff8057df108
Raul Padilla <raulpadilla112@gmail.com>
2019-05-14 15:41:54 -0400

    Project that shows functions working with other functions.

---
+++ Thing-a-Ma-JingProject/thingamajig.js
@@ -0,0 +1,34 @@
+function clunk(times){
+    var num = times;
+    while(num> 0){
+        display("clunk");
+        num = num -1;
+    }
+}
+
+function thingamajig(size){
+    var facky=1;
+    clunkCounter = 0;
+    if(size == 0){
+        display("clank");
+    }else if(size == 1){
+        display("thunk");
+    }else{
+        while(size>1){
+            facky = facky * size;
+            size = size -1;
+        }
+        clunk(facky);
+    }
+}
+
+function display(output){
+    console.log(output);
+    clunkCounter = clunkCounter + 1;
+}
+
+var clunkCounter = 0;
+for(var i = 0; i<6; i++){
+    thingamajig(i);
+}
+console.log(clunkCounter);
