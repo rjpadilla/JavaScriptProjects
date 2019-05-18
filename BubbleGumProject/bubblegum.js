66676e03cb8fe3aae874366cac31e95b9e641a8f
Raul Padilla <raulpadilla112@gmail.com>
2019-05-18 02:26:32 -0400

    Changed while to a for loop.

--- BubbleGumProject/bubblegum.js
+++ BubbleGumProject/bubblegum.js
@@ -4,9 +4,8 @@ var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 
 var hasBubbleGum = [false, false, false, true];
 
-while(i< hasBubbleGum.length){
-    if(hasBubbleGum[i]){
+for (i; i < hasBubbleGum.length; i++) {
+    if (hasBubbleGum[i]) {
         console.log(products[i] + " contains bubble gum");
     }
-    i = i +1;
 }
