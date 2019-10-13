24f6ac8321eb79494422f7d8458829545139fddd
Raul Padilla <raulpadilla112@gmail.com>
2019-10-13 19:55:53 -0400

    Updated function.

--- JumpProject/scripts/my_scripts.js
+++ JumpProject/scripts/my_scripts.js
@@ -1,17 +1,31 @@
 
 
 $(document).ready(function(){
-    $(".guess_box").click(function(){
-        $(".guess_box p").remove();
-        var discount = Math.floor((Math.random()*5) + 5);
-        var discount_msg = "<p>Your Discount is " + discount +"%</p>";
-        //alert(discount); //this pops up alert box
-        $(this).append(discount_msg);
-
-        //loop
+    $(".guess_box").click(checkForCode);
+    function checkForCode(){
+        var my_num = Math.floor((Math.random() * 5) + 5);
+        var discount = "<p>Your Discount is " +my_num+"%</p>";
+        $(this).append(discount);
+       
         $(".guess_box").each(function(){
-            $(this).unbind("click");
+            $(this).unbind('click');
         });
+    }
+
+
+
+
+    // $(".guess_box").click(function(){
+    //     $(".guess_box p").remove();
+    //     var discount = Math.floor((Math.random()*5) + 5);
+    //     var discount_msg = "<p>Your Discount is " + discount +"%</p>";
+    //     //alert(discount); //this pops up alert box
+    //     $(this).append(discount_msg);
+
+    //     //loop
+    //     $(".guess_box").each(function(){
+    //         $(this).unbind("click");
+    //     });
         
-    });//end click function
+    // });//end click function
 });//end doc ready
