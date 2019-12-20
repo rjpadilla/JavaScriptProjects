470ddf696ddf9bf071dcb5a4b694745c6e13c88e
Raul Padilla <raulpadilla112@gmail.com>
2019-12-20 15:42:30 -0500

    Updated hand.

--- BlackJackProject/scripts/my_scripts.js
+++ BlackJackProject/scripts/my_scripts.js
@@ -1,4 +1,4 @@
-function card(name, suit, value){
+function card(name, suit, value) {
     this.name = name;
     this.suit = suit;
     this.value = value;
@@ -65,32 +65,44 @@ var deck = [
 var used_cards = new Array();
 var hand = {
     cards: new Array(),
-    current_total : 0,
-    sumCardTotal: function(){
+    current_total: 0,
+    sumCardTotal: function () {
         this.current_total = 0;
-        for(var i=0; i<this.cards.length; i++){
+        for (var i = 0; i < this.cards.length; i++) {
             var c = this.cards[i];
             this.current_total += c.value;
         }
         $("#hdrTotal").html("Total: " + this.current_total);
+        if (this.current_total > 21) {
+            $("#btnStick").trigger("click");
+            $("#hdrResult").html("BUST!");
+        } else if (this.current_total == 21) {
+            $("#btnStick").trigger("click");
+            $("#hdrResult").html("BlackJack!");
+        } else if (this.current_total <= 21 && this.cards.length == 5) {
+            $("#btnStick").trigger("click");
+            $("#hdrResult").html("BlackJack - 5 card trick!");
+        } else{
+            //Keep Playing! :D
+        }
     }
 };
-function deal(){
-    for(var i =0; i<2; i++){
+function deal() {
+    for (var i = 0; i < 2; i++) {
         hit();
     }
 }
 
-function getRandom(num){
+function getRandom(num) {
     var my_num = Math.floor(Math.random() * num);
     return my_num;
 }
 
-function hit(){
+function hit() {
     var good_card = false;
-    do{
+    do {
         var index = getRandom(52);
-        if( !$.inArray(index, used_cards) > -1){
+        if (!$.inArray(index, used_cards) > -1) {
             good_card = true;
             var c = deck[index];
             used_cards[used_cards.length] = index;
@@ -99,11 +111,35 @@ function hit(){
             $d.addClass("current_hand").appendTo("#my_hand");
             $("<img>").appendTo($d).attr('src', 'images/cards/' + c.suit + '/' + c.name + '.jpg').fadeout('slow').fadeIn('slow');
         }
-    }while(!good_card);
+    } while (!good_card);
     good_card = false;
+    hand.sumCardTotal();
 }
 
 $("#btnDeal").click(function(){
     deal();
     $(this).toggle();
+    $("#btnHit").toogle();
+    $("#btnStick").toggle();
+});
+
+$("btnHit").click(function(){
+    hit();
+});
+
+$("btnStick").click(function(){
+    $("hdrResult").html('Stick!');
+    $("result").toggle();
+});
+
+$("#btnRestart").click(function(){
+    $("#result").toggle();
+    $(this).toggle();
+    $("#my_hand").empty();
+    $("#hdrResult").html('');
+    used_cards.length = 0;
+    hand.cards.length = 0;
+    hand.current_total = 0;
+
+    $("btnDeal").toggle().trigger('click');
 });
