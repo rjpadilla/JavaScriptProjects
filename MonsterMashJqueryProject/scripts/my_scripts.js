90b7ce459747dc6d4dea58b4ae54090a66b8a45c
Raul Padilla <raulpadilla112@gmail.com>
2019-12-23 18:55:43 -0500

    Made a custom function for face animation.

--- MonsterMashJqueryProject/scripts/my_scripts.js
+++ MonsterMashJqueryProject/scripts/my_scripts.js
@@ -1,49 +1,30 @@
 $(document).ready(function () {
-    var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0;
-    lightning_one(4000);
-    lightning_two(5000);
-    lightning_three(7000);
-    $("#head").click(function () {
-        if (headclix < 9) {
-            $(this).animate({left:"-=367px"},500);
-            headclix += 1;
-        }
-        else {
-            $(this).animate({left:"0px"},500);
-            headclix = 0;
-        }
-    });
-    $("#eyes").click(function(){
-        if(eyeclix < 9){
-            $(this).animate({left:"-=367px"},500);
-            eyeclix += 1;
-        }
-        else{
-            $(this).animate({left:"0px"},500);
-            eyeclix = 0;
-        }
-    });
-    $("#nose").click(function(){
-        if(noseclix < 9){
-            $(this).animate({left:"-=367px"},500);
-            noseclix += 1;
-        }
-        else{
-            $(this).animate({left:"0px"},500);
-            noseclix = 0;
-        }
-    });
-    $("#mouth").click(function(){
-        if(mouthclix < 9){
-            $(this).animate({left:"-=367px"},500);
-            mouthclix += 1;
-        }
-        else{
-            $(this).animate({left:"0px"},500);
-            mouthclix = 0;
-        }
+    var clix = [0,0,0,0];
+    $("#head").click(function(){
+        moveMe(0,this);
     });
 
+    $("#eyes").click(function(){
+        moveMe(1,this);
+    });
+
+    $("#nose").click(function(){
+        moveMe(2,this);
+    });
+
+    $("#mouth").click(function(){
+        moveMe(3,this);
+    });
+
+    function moveMe(i,obj){
+        if(clix[i] < 9){
+            $(obj).animate({left:"-=367px"},500);
+            clix[i] = clix[i] +1;
+        }else{
+            clix[i]=0;
+            $(obj).animate({left:"0px"},500);
+        }
+    }
     
 });
 
